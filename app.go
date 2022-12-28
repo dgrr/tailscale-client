b536f5b9a8df127b995fb132ce07136541cb4f6d
Dario <me@dgrp.es>
2022-12-28 10:44:45 +0100

    UI stuff + some backend logic

--- app.go
+++ app.go
@@ -3,12 +3,17 @@ package main
 import (
 	"context"
 	"log"
+	"net/netip"
 	"strings"
+	"time"
 
 	"github.com/dgrr/tl"
 	"github.com/gen2brain/beeep"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 	"tailscale.com/client/tailscale"
 	"tailscale.com/ipn"
+	"tailscale.com/ipn/ipnstate"
+	"tailscale.com/types/key"
 )
 
 // App struct
@@ -24,13 +29,42 @@ func NewApp() *App {
 
 // startup is called when the app starts. The context is saved
 // so we can call the runtime methods
-func (a *App) startup(ctx context.Context) {
-	a.ctx = ctx
+func (app *App) startup(ctx context.Context) {
+	app.ctx = ctx
 	beeep.Notify("Tailscale", "tailscale started", "icon/on.png")
+
+	go app.watchFiles()
+	go app.watchIPN()
 }
 
-func (a *App) CurrentAccount() string {
-	current, _, err := a.client.ProfileStatus(a.ctx)
+func (app *App) watchFiles() {
+
+}
+
+func (app *App) watchIPN() {
+	for {
+		watcher, err := app.client.WatchIPNBus(app.ctx, 0)
+		if err != nil {
+			log.Printf("loading IPN bus watcher: %s\n", err)
+			time.Sleep(time.Second)
+		}
+
+		for {
+			not, err := watcher.Next()
+			if err != nil {
+				log.Printf("Watching IPN Bus: %s\n", err)
+				break
+			}
+
+			runtime.EventsEmit(app.ctx, "update_all")
+
+			log.Printf("IPN bus update: %v\n", not)
+		}
+	}
+}
+
+func (app *App) CurrentAccount() string {
+	current, _, err := app.client.ProfileStatus(app.ctx)
 	if err != nil {
 		panic(err)
 	}
@@ -38,8 +72,29 @@ func (a *App) CurrentAccount() string {
 	return current.Name
 }
 
-func (a *App) Accounts() []string {
-	current, all, err := a.client.ProfileStatus(a.ctx)
+func (app *App) SetExitNode(dnsName string) {
+	status, err := app.client.Status(app.ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	peers := status.Peers()
+
+	i := tl.SearchFn(peers, func(nodeKey key.NodePublic) bool {
+		peer := status.Peer[nodeKey]
+		return peer.DNSName == dnsName
+	})
+	if i == -1 {
+		return
+	}
+
+	peer := status.Peer[peers[i]]
+	// TODO: get prefs, set prefs
+	_ = peer
+}
+
+func (app *App) Accounts() []string {
+	current, all, err := app.client.ProfileStatus(app.ctx)
 	if err != nil {
 		panic(err)
 	}
@@ -62,13 +117,65 @@ type Namespace struct {
 }
 
 type Peer struct {
-	Name           string `json:"name"`
-	ExitNode       bool   `json:"exit_node"`
-	ExitNodeOption bool   `json:"exit_node_option"`
+	DNSName        string    `json:"dns_name"`
+	Name           string    `json:"name"`
+	ExitNode       bool      `json:"exit_node"`
+	ExitNodeOption bool      `json:"exit_node_option"`
+	Online         bool      `json:"online"`
+	OS             string    `json:"os"`
+	Addrs          []string  `json:"addrs"`
+	Routes         []string  `json:"routes"` // primary routes
+	IPs            []string  `json:"ips"`
+	Created        time.Time `json:"created_at"`
+	LastSeen       time.Time `json:"last_seen"`
 }
 
-func (a *App) Peers() []Namespace {
-	status, err := a.client.Status(a.ctx)
+func (app *App) Self() Peer {
+	status, err := app.client.Status(app.ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	self := status.Self
+	return convertPeer(self)
+}
+
+func convertPeer(status *ipnstate.PeerStatus) Peer {
+	peerName, _ := splitPeerNamespace(status.DNSName)
+	return Peer{
+		DNSName:        status.DNSName,
+		Name:           peerName,
+		ExitNode:       status.ExitNode,
+		ExitNodeOption: status.ExitNodeOption,
+		Online:         status.Online,
+		OS:             status.OS,
+		Addrs:          status.Addrs,
+		Created:        status.Created,
+		LastSeen:       status.LastSeen,
+		Routes: func() []string {
+			if status.PrimaryRoutes == nil {
+				return nil
+			}
+
+			return tl.Map(status.PrimaryRoutes.AsSlice(), func(prefix netip.Prefix) string {
+				return prefix.String()
+			})
+		}(),
+		IPs: tl.Map(status.TailscaleIPs, func(ip netip.Addr) string {
+			return ip.String()
+		}),
+	}
+}
+
+func splitPeerNamespace(dnsName string) (peerName, namespace string) {
+	names := strings.Split(dnsName, ".")
+	namespace = strings.Join(names[1:], ".")
+	peerName = names[0]
+	return peerName, namespace
+}
+
+func (app *App) Peers() []Namespace {
+	status, err := app.client.Status(app.ctx)
 	if err != nil {
 		panic(err)
 	}
@@ -77,14 +184,9 @@ func (a *App) Peers() []Namespace {
 
 	for _, nodeKey := range status.Peers() {
 		tsPeer := status.Peer[nodeKey]
-		dnsName := strings.Split(tsPeer.DNSName, ".")
-		namespace := strings.Join(dnsName[1:], ".")
-		peerName := dnsName[0]
-		peer := Peer{
-			Name:           peerName,
-			ExitNode:       tsPeer.ExitNode,
-			ExitNodeOption: tsPeer.ExitNodeOption,
-		}
+		_, namespace := splitPeerNamespace(tsPeer.DNSName)
+
+		peer := convertPeer(tsPeer)
 
 		i := tl.SearchFn(res, func(a Namespace) bool {
 			return namespace == a.Name
@@ -104,8 +206,8 @@ func (a *App) Peers() []Namespace {
 	return res
 }
 
-func (a *App) SwitchTo(account string) {
-	current, all, err := a.client.ProfileStatus(a.ctx)
+func (app *App) SwitchTo(account string) {
+	current, all, err := app.client.ProfileStatus(app.ctx)
 	if err != nil {
 		panic(err)
 	}
@@ -122,5 +224,5 @@ func (a *App) SwitchTo(account string) {
 		return
 	}
 
-	a.client.SwitchProfile(a.ctx, all[0].ID)
+	app.client.SwitchProfile(app.ctx, all[0].ID)
 }
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <script>
-import {Accounts, CurrentAccount, Peers, SwitchTo} from "../wailsjs/go/main/App";
+import {Accounts, CurrentAccount, Peers, Self, SwitchTo} from "../wailsjs/go/main/App";
+import {EventsOn} from "../wailsjs/runtime";
 
 export default {
   data() {
@@ -7,13 +8,17 @@ export default {
       account: '',
       other_accounts: [],
       peers: [],
+      self: {},
+      selected_peer: {},
     }
   },
   methods: {
     load: async function() {
       this.account = await CurrentAccount();
       this.other_accounts = await Accounts();
+      this.self = await Self();
       this.peers = await Peers();
+      this.selected_peer = this.self;
       console.log(this.peers)
     },
     switchAccount: async function(event) {
@@ -21,28 +26,47 @@ export default {
       console.log("switch to", name);
       await SwitchTo(name)
       await this.load()
-    }
+    },
   },
   mounted() {
-    this.load()
+    this.load();
+    this.timer = setInterval(() => {
+      this.load()
+    }, 5000);
+
+    EventsOn('update_all', () => this.load())
+  },
+  unmounted() {
+    clearInterval(this.timer)
   }
 }
 </script>
 
 <template>
   <div class="flex h-screen">
-    <div class="w-1/3 h-full border-solid border-2 border-sky-500">
-      <div class="overflow-y-auto py-4 px-3 rounded">
+    <div class="w-1/3 h-full border-left-solid border-2 border-l-0 border-t-0 border-b-0 border-sky-500 overflow-scroll">
+      <div class="py-4 px-3 rounded">
         <ul class="list-disc space-y-2">
+          <li>
+            <div class="flex-1 min-w-0">
+              <a href="#" @click="selected_peer = self" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                <p class="text-md font-medium text-gray-900 truncate dark:text-white">
+                  This machine
+                </p>
+              </a>
+            </div>
+          </li>
+        </ul>
+        <ul class="list-disc pt-5 space-y-2">
           <li v-for="namespace in peers">
             <p class="text-md font-medium text-gray-900 truncate dark:text-white">
               <span class="ml-3">{{ namespace.name }}</span>
             </p>
             <ol class="pl-5 mt-2 space-y-1 list-inside">
               <li v-for="peer in namespace.peers">
-                <div class="flex items-center space-x-3">
+                <div class="flex items-center space-x-0">
                   <div class="flex-1 min-w-0">
-                    <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <a href="#" @click="selected_peer = peer" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
                       <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
                         {{ peer.name }}
                       </p>
@@ -50,8 +74,23 @@ export default {
                   </div>
                   <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
                     <!-- https://flowbite.com/docs/components/badge/ -->
-                    <span v-if="peer.exit_node_option" class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
+                    <span v-if="peer.exit_node" class="bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-800">
                       Exit node
+                    </span>
+                    <span v-else-if="peer.exit_node_option" class="bg-yellow-100 text-yellow-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-200 dark:text-yellow-800">
+                      Exit node
+                    </span>
+                  </div>
+                  <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                    <!-- https://flowbite.com/docs/components/badge/ -->
+                    <span v-if="peer.online" class="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-800">
+                      Online
+                    </span>
+                  </div>
+                  <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                    <!-- https://flowbite.com/docs/components/badge/ -->
+                    <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
+                      {{ peer.os }}
                     </span>
                   </div>
                 </div>
@@ -95,6 +134,38 @@ export default {
             </MenuItems>
           </transition>
         </Menu>
+      </div>
+      <div class="flex flex-col mt-20 justify-center items-center px-2">
+        <div>
+          <h2 class="mt-8 text-center text-2xl font-bold text-zinc-100 cursor-default select-none"> {{ selected_peer.name }} </h2>
+          <div class=" text-sm text-zinc-300">
+            <p class="text-center"> {{ selected_peer.dns_name }} </p>
+          </div>
+        </div>
+        <div class="w-full sm:max-w-md px-6 mt-2 mb-8">
+          <div class="mt-8 space-y-8">
+            <div class="mt-8 space-y-8 pl-4 pr-4 pt-4 pb-4 rounded border border-black-200 dark:border-white-700">
+              <div v-for="ip in selected_peer.ips" class="flex items-center justify-between">
+                <p class="text-center text-md font-medium text-gray-900 truncate dark:text-white">
+                  {{ ip }}
+                </p>
+                <button type="button" class="text-blue-700 border border-blue-700 bg-white hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800">
+                  <svg height="16" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg"><path d="m0 3c0-1.644531 1.355469-3 3-3h5c1.644531 0 3 1.355469 3 3 0 .550781-.449219 1-1 1s-1-.449219-1-1c0-.570312-.429688-1-1-1h-5c-.570312 0-1 .429688-1 1v5c0 .570312.429688 1 1 1 .550781 0 1 .449219 1 1s-.449219 1-1 1c-1.644531 0-3-1.355469-3-3zm5 5c0-1.644531 1.355469-3 3-3h5c1.644531 0 3 1.355469 3 3v5c0 1.644531-1.355469 3-3 3h-5c-1.644531 0-3-1.355469-3-3zm2 0v5c0 .570312.429688 1 1 1h5c.570312 0 1-.429688 1-1v-5c0-.570312-.429688-1-1-1h-5c-.570312 0-1 .429688-1 1zm0 0" fill="#2e3436"/></svg>
+                  <span class="sr-only">Icon description</span>
+                </button>
+              </div>
+            </div>
+            <div class="flex items-center justify-between">
+              <p class="text-center text-md font-medium text-gray-900 truncate dark:text-white">
+                Use exit node
+              </p>
+              <label class="inline-flex relative items-center cursor-pointer">
+                <input type="checkbox" value="" class="sr-only peer">
+                <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+              </label>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
